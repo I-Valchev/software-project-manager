aeda58e0aded8a55bd37734e2526ac161608d85f
ivovalchev <Ivo.V.Valchev@gmail.com>
2016-03-15 19:59:41 +0200

    #22 Removing previous implementation

--- pm/src/main/webapp/js/project_managers.js
+++ pm/src/main/webapp/js/project_managers.js
@@ -16,19 +16,6 @@ $(document).ready(function() {
     	project_managers.empty();
     }
     
-    /*function addProjectManagerToContent(projectManager){
-    	var content = '<div class="col-xs-12 col-sm-6 col-md-6"> <div class="well well-sm"> <div class="row"> <div class="col-sm-6 col-md-4"> <img src="http://placehold.it/380x500" alt="" class="img-rounded img-responsive" /> </div> <div class="col-sm-6 col-md-8"> <h4 id="project-manager-name-'+projectManager.id+'"></h4> <small><cite>Sample place<i id="profile_glyph" class="glyphicon glyphicon-map-marker"> </i> </cite></small> <p> <i class="glyphicon glyphicon-envelope"></i>email@example.com <br /> <i class="glyphicon glyphicon-globe"></i><a href="http://www.jquery2dotnet.com">www.sample.com</a> <br /> <i class="glyphicon glyphicon-gift"></i>01.01.1970 </p> <div class="btn-group"> <button type="button" class="btn btn-primary">Projects</button> <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown"> <span class="caret"></span><span class="sr-only">Social</span> </button> <ul class="dropdown-menu" role="menu" id="list-projects"></ul> </div> </div> </div> </div> </div>'
-    	project_managers.append(content);
-    	
-    	$("#project-manager-name-"+projectManager.id).text(projectManager.id);
-    	
-    	var project_link = ENDPOINT_PROJECT_MANAGERS+'/'+ projectManager.id;
-    	
-    	$.each(projectManager.projectIds, function(index, value){
-    		$("#list-projects").append($('<li></li>').append($('<a></a>').attr('href', project_link).text(value)));
-    	})
-    }*/
-    
     function addProjectManagerToContent(projectManager){
     	var main_div = $("<div class='col-xs-12 col-sm-6 col-md-6'></div>");
     	var well_div = $("<div class='well well-sm'></div>");
