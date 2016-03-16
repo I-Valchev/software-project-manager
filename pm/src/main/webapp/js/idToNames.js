33538c43323a3321f00bf1819c41f4c9956c178d
ivovalchev <Ivo.V.Valchev@gmail.com>
2016-03-16 11:12:45 +0200

    #17 getDeveloper(id) and getDeveloperUsername(id) are now implemented

--- pm/src/main/webapp/js/idToNames.js
+++ pm/src/main/webapp/js/idToNames.js
@@ -1,8 +1,32 @@
 var ENDPOINT_USERS = "http://localhost:3000/users";
+var ENDPOINT_DEVELOPERS = "http://localhost:3000/developers";
+
+function endpointUsers(id){
+	return ENDPOINT_USERS + "/" + id;
+}
+
+function endpointDevelopers(id){
+	return ENDPOINT_DEVELOPERS + "/" + id;
+}
 
 function requestUser(id) {
-	return $.ajax(ENDPOINT_USERS + "/" + id, {
+	return $.ajax(endpointUsers(id), {
 		method : "GET",
 		dataType : "json"
 	});
+}
+
+function getDeveloper(id){
+	return $.ajax(endpointDevelopers(id), {
+		method : "GET",
+		dataType : "json"
+	});
+}
+
+function getDeveloperUsername(developerId){
+	getDeveloper(developerId).then(function(response){
+		getUsers(response).then(function(user){
+			return user.username
+		})
+	})
 }
