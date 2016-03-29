e6ec01a82ddc7a19545dbec885c65c2bd28eafc8
ivovalchev <Ivo.V.Valchev@gmail.com>
2016-03-29 09:35:53 +0300

    #30 Getting project

--- pm/src/main/java/rest/ProjectRest.java
+++ pm/src/main/java/rest/ProjectRest.java
@@ -5,6 +5,7 @@ import java.util.List;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -24,5 +25,12 @@ public class ProjectRest {
 	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
 	public List<Project> getProject(){
 		return projectsService.getProjects();
+	}
+	
+	@GET
+	@Path("/{projectId}")
+	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	public Project getProject(@PathParam("projectId") long projectId){
+		return projectsService.getProject(projectId);
 	}
 }
