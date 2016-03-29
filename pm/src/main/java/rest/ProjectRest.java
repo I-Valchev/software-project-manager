6f9628a2d026eda45e0163b650af4e593e414b68
ivovalchev <Ivo.V.Valchev@gmail.com>
2016-03-29 09:34:12 +0300

    #30 Initial Project rest - listing all projects

---
+++ pm/src/main/java/rest/ProjectRest.java
@@ -0,0 +1,28 @@
+package rest;
+
+import java.util.List;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import entities.Project;
+import services.ProjectsService;
+
+@Path("/projects")
+public class ProjectRest {
+	private final ProjectsService projectsService;
+
+	@Inject
+	public ProjectRest(ProjectsService projectsService){
+		this.projectsService = projectsService;
+	}
+	
+	@GET
+	@Produces({MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML})
+	public List<Project> getProject(){
+		return projectsService.getProjects();
+	}
+}
