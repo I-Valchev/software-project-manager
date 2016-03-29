17b8c5a4614b3644bdfcb2e95a8dd0a6c05b3b22
ivovalchev <Ivo.V.Valchev@gmail.com>
2016-03-29 09:22:39 +0300

    #29 Getting all projects

--- pm/src/main/java/services/ProjectsService.java
+++ pm/src/main/java/services/ProjectsService.java
@@ -1,8 +1,11 @@
 package services;
 
+import java.util.List;
+
 import javax.inject.Inject;
 import javax.inject.Singleton;
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
 import entities.Project;
 
@@ -24,6 +27,19 @@ public class ProjectsService {
 			em.getTransaction().commit();
 
 			return project;
+		}finally{
+			if(em.getTransaction().isActive()){
+				em.getTransaction().rollback();
+			}
+			em.close();
+		}
+	}
+	
+	public List<Project> getProjects(){
+		final EntityManager em = entityManagerService.createEntityManager();
+		try{
+			final TypedQuery<Project> query = em.createNamedQuery(Project.QUERY_ALL, Project.class);
+			return query.getResultList();
 		}finally{
 			if(em.getTransaction().isActive()){
 				em.getTransaction().rollback();
